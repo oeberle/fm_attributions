e6fec0598acbede21321fb9e45e7252bf03d966d
Oliver Eberle <oliver.eberle@tu-berlin.de>
2025-04-10 10:54:15 +0200

    setup

--- setup_colab.sh
+++ setup_colab.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo "$PWD"
+
 pip install zennit
 pip install captum
 
@@ -9,9 +11,9 @@ wget "https://git.tu-berlin.de/pchormai/drsa-demo/-/raw/main/tests/data/castle.j
 
 
 ## Get Weights
-cd prompt-me-a-dataset/dino_files/weights && wget -q https://github.com/IDEA-Research/GroundingDINO/releases/download/v0.1.0-alpha/groundingdino_swint_ogc.pth && cd ..
+cd /content/prompt-me-a-dataset/dino_files/weights && wget -q https://github.com/IDEA-Research/GroundingDINO/releases/download/v0.1.0-alpha/groundingdino_swint_ogc.pth && cd ..
 
-cd prompt-me-a-dataset/sam_files/weights &&  wget -q https://dl.fbaipublicfiles.com/segment_anything/sam_vit_h_4b8939.pth && cd ..
+cd /content/prompt-me-a-dataset/sam_files/weights &&  wget -q https://dl.fbaipublicfiles.com/segment_anything/sam_vit_h_4b8939.pth && cd ..
 
 
 pip install git+https://github.com/facebookresearch/segment-anything.git
@@ -19,4 +21,7 @@ pip install git+https://github.com/facebookresearch/segment-anything.git
 pip install groundingdino-py
 
 # Get Data
+
+echo "$PWD"
+
 wget https://tubcloud.tu-berlin.de/s/ZCr7YxqyBX598cY/download/1808_clavius_sphaera_1585.zip && unzip 1808_clavius_sphaera_1585.zip  && rm -rf __MACOSX/ && rm 1808_clavius_sphaera_1585.zip
